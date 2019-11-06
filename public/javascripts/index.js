394a94b99714a02810a221f92c280f83c6cc2441
stupidJoon <junsang.yu3@gmail.com>
2019-11-06 22:19:39 +0900

    a

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,6 +2,6 @@ $(document).ready(() => {
   let createSessionButton = $("#create_session");
 
   createSessionButton.click(() => {
-    location.href = 'session';
+    window.location.href = "session";
   });
 });
